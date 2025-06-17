701707da39bbbac44f9b6365a36415c43c15bf48
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-06-17 09:16:40 -0500

    play with examples of arrow types and ordered pairs

--- simple_type_theory.lean
+++ simple_type_theory.lean
@@ -18,3 +18,28 @@ def b2 : Bool := false
 #eval 5 * 4
 #eval m + 3
 #eval b1 && b2
+
+
+-- arrow types (which ARE TYPES)
+#check Nat → Nat
+#check Nat -> Nat
+#check Nat × Nat
+#check Nat → Nat → Nat
+
+-- things that ARE arrow or pair types
+#check Nat.succ
+#check (0, 1)
+#check Nat.add
+
+-- types of functional expressions
+#check Nat.succ 2
+#check Nat.add 3
+#check Nat.add 4 5
+#check (5, 9).fst
+#check (5, 9).snd
+
+-- eval basic arrow and pair expressions
+#eval Nat.succ 2
+#eval Nat.add 5 2
+#eval (5, 9).1
+#eval (5, 9).2
